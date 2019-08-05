9b733c5e0af874a699c3220f6d450bea935b274b
Alice Brunel <alc.brunel@gmail.com>
2019-08-05 21:19:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,8 +15,11 @@ def create_project_hash
   projects = {}
   
   # Iterate through projects
+    kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
   
-  
+  projects
 end
 
 create_project_hash
